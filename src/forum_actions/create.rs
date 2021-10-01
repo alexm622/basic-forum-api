a18634a03626104c40289c23d7800a5288eaf92d
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-09-30 21:03:38 -0400

    more comments

--- src/forum_actions/create.rs
+++ src/forum_actions/create.rs
@@ -3,70 +3,96 @@ pub mod create{
     use crate::structs::requests::post::{MakeCat, MakeComment,MakePost};
     use crate::database::insert::insert;
     use crate::database::get::get;
-    pub fn create_post(req: MakePost, ip:String)-> StatusResponse{
-        let mut res:StatusResponse = StatusResponse{
-            response_code: 1,
-            redirect: None,
-        };
-        let good_token:bool = get::verify_token(req.user_token.clone(), ip, req.user).unwrap();
-        let good_parent:bool = get::check_cat(req.cat).unwrap();
-        if good_token & good_parent{
-            let post_id = insert::create_post(req).unwrap();
-        res.redirect = Some(post_id.to_string().to_owned());
-        }else{
-            if good_token {
-                res.redirect = Some("exists".to_owned());
-            }else{
-                res.redirect = Some("bad token+ip combo".to_owned());
-            }
-        }
-        
-        return res;
-    }
+
+    //create a new category
     pub fn create_category(req: MakeCat, ip:String)-> StatusResponse{
+        //create the status response
         let mut res:StatusResponse = StatusResponse{
             response_code: 1,
             redirect: None,
         };
+        //check to see if the token is valid
         let good_token:bool = get::verify_token(req.user_token.clone(), ip, req.user).unwrap();
+        //see if category exists
         let exists:bool = get::cat_exists(req.name.clone()).unwrap();
+        //if the token is good and the category does not exist create the category
         if good_token & !exists{
             let cat_id:u64 = insert::create_cat(req).unwrap();
+            //set the status to have the cat id
             res.redirect = Some(cat_id.to_string().to_owned());
         }else{
             if good_token {
+                //the token is good, but category exists
                 res.redirect = Some("exists".to_owned());
             }else{
                 res.redirect = Some("bad token+ip combo".to_owned());
             }
         }
-        
+        //return the new response
         return res;
     }
+    
+    //create a post
+    pub fn create_post(req: MakePost, ip:String)-> StatusResponse{
+        //create the status response
+        let mut res:StatusResponse = StatusResponse{
+            response_code: 1,
+            redirect: None,
+        };
+        //check to see if token is valid
+        let good_token:bool = get::verify_token(req.user_token.clone(), ip, req.user).unwrap();
+        //check to see if category is valid
+        let good_parent:bool = get::check_cat(req.cat).unwrap();
 
+        //if all tests pass create the post
+        if good_token & good_parent{
+            let post_id = insert::create_post(req).unwrap();
+            res.redirect = Some(post_id.to_string().to_owned());
+        }else{
+            //if the token is good then the category does not exist
+            if good_token {
+                res.redirect = Some("category does not exist".to_owned());
+            }else{
+                res.redirect = Some("bad token+ip combo".to_owned());
+            }
+        }
+        //return the http response
+        return res;
+    }
+    //create a new comment
     pub fn create_comment(req: MakeComment, ip:String) -> StatusResponse{
         let mut res:StatusResponse = StatusResponse{
             response_code: 1,
             redirect: None,
         };
-        
+        //check if token is valid
         let good_token:bool = get::verify_token(req.user_token.clone(), ip, req.user).unwrap();
+        //check to see if post exists
         let good_post:bool = get::check_post(req.post).unwrap();
+        //check to see if the parent comment is good
         let good_parent:bool = get::check_comment(req.parent).unwrap();
+
+        //if all tests pass then create comment
         if good_token & good_parent & good_post{
             let comment_id:u64 = insert::create_comment(req).unwrap();
+            //return the comment id in status
             res.redirect = Some(comment_id.to_string().to_owned());
         }else{
+            //if the token is good then figure out what failed
             if good_token {
+                //if the post is good then the parent comment is bad
                 if good_post{
                     res.redirect = Some("bad parent".to_owned());
                 }else{
+                    //post is bad
                     res.redirect = Some("bad post".to_owned());
                 }
             }else{
+                //token + ip is bad
                 res.redirect = Some("bad token+ip combo".to_owned());
             }
         }
+        //return the new response
         return res;
     }
 }
--- src/utils/ip_tools.rs
+++ src/utils/ip_tools.rs
@@ -1,7 +1,12 @@
 pub mod ip_tools{
+    //TODO: optimize this for ipv6
+    //strip the port off of the ip request
     pub fn strip_port(ip:String)->String{
+        //split on ":"
         let split = ip.split(":");
+        //collect into a vector
         let vec = split.collect::<Vec<&str>>();
+        //return the stripped ip
         return vec[0].to_owned();
     }
 }
