1897df20ca6119a33b5a0616e88afd910b94d778
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-09-30 21:03:00 -0400

    added a ton of comments

--- src/database/get.rs
+++ src/database/get.rs
@@ -6,13 +6,20 @@ pub mod get{
     use crate::database::insert::insert;
     use crate::utils::ip_tools::ip_tools;
 
+    //the mysql server url
     const URL:&str = "mysql://server:serverpass@10.16.40.202:3306/forum";
 
+    //get the id info for a username
     pub fn get_id_info(uname: String) -> Result<IdInfo>{
+        //create the connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //create the statement
         let stmt = conn.prep("SELECT user_id, auth_id, moderation_id FROM users WHERE username = :uname")?;
+
+        //query the server
         let res:Vec<IdInfo> = conn.exec_map(stmt, params!{
             "uname" => uname.clone(),
         }, |(user_id, auth_id, moderation_id)|
@@ -21,88 +28,108 @@ pub mod get{
                 uid: user_id,
                 mid: moderation_id,
             }).expect("Query failed.");
+        //if there is more than one response or there is 0 responses return a default
         if res.len() != 1{
-            log::error!("There are more than one entries for username {}", uname);
+            log::error!("There are more than one entries for username {} or that username does not exist", uname);
+            //create the output struct
             let out:IdInfo = IdInfo{
                 aid: 0,
                 uid: 0,
                 mid: 0,
             };
+            //return the struct
             Ok(out)
         }else{
+            //return the only result
             Ok(res[0].clone())
         }
         
     }
+    //check to see if token is not taken/currently in use
+    // if not initialize said token
     pub fn check_token(token:String, aid:u64, ip:String) -> Result<bool>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //prepare the sql query
         let stmt = conn.prep("SELECT auth_id FROM auth WHERE active_token = :token")?;
+
+        //test to see if token exists
         let res:Vec<u64> = conn.exec(stmt, params!{
             "token" => token.clone(),
         }).expect("Query failed.");
+        //if token already exists then try for a new token
         if res.len() != 0{
             Ok(false)
         }else{
+            //token does not exist so initialize it
             insert::add_token(token, aid, ip).unwrap();
             Ok(true)
         }
         
     }
+
+    //check to see if category exists
     pub fn check_cat(id:u64) -> Result<bool>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //prepare sql query
         let stmt = conn.prep("SELECT cat_id FROM categories WHERE cat_id = :id")?;
+
+        //query database to see if category id exists
         let res:Vec<u64> = conn.exec(stmt, params!{
             "id" => id,
         }).expect("Query failed.");
+        //if it does not exist return false
+        //if it does exist return true
         if res.len() == 0{
             Ok(false)
         }else{
             Ok(true)
         } 
     }
+    //check to see if a category exists
     pub fn cat_exists(name:String) -> Result<bool>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //prepare sql query
         let stmt = conn.prep("SELECT cat_name FROM categories WHERE cat_name = :name")?;
+
+        //query the server
         let res:Vec<u64> = conn.exec(stmt, params!{
             "name" => name,
         }).expect("Query failed.");
+
+        //if the name does not exist return false
         if res.len() == 0{
             Ok(false)
         }else{
+            //the category exists so we're returning true
             Ok(true)
         } 
     }
+    //check to see if post exists
     pub fn check_post(id:u64) -> Result<bool>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //prepare the sql query
         let stmt = conn.prep("SELECT post_id FROM posts WHERE post_id = :id")?;
         let res:Vec<u64> = conn.exec(stmt, params!{
             "id" => id,
         }).expect("Query failed.");
-        if res.len() == 0{
-            Ok(false)
-        }else{
-            Ok(true)
-        } 
-    }
-    pub fn check_comment(id:u64) -> Result<bool>{
-        let opts = Opts::from_url(URL).unwrap();
-        let pool = Pool::new(opts).unwrap();
-        let mut conn = pool.get_conn().unwrap();
-        if id == 0{
-            return Ok(true);
-        }
-        let stmt = conn.prep("SELECT comment_id FROM comments WHERE comment_id = :id")?;
-        let res:Vec<u64> = conn.exec(stmt, params!{
-            "id" => id,
-        }).expect("Query failed.");
+
+        //if post id does not exist return false, but if it exists then return true
         if res.len() == 0{
             Ok(false)
         }else{
@@ -110,35 +137,81 @@ pub mod get{
         } 
     }
 
-    pub fn  verify_token(token:String, ip:String, uid_test:u64)-> Result<bool>{
+    //check to see if comment with id exists
+    pub fn check_comment(id:u64) -> Result<bool>{
+        //if the parent comment does not exist then it is the first comment
+        if id == 0{
+            return Ok(true);
+        }
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+        
+        //prepare SQL query
+        let stmt = conn.prep("SELECT comment_id FROM comments WHERE comment_id = :id")?;
+        let res:Vec<u64> = conn.exec(stmt, params!{
+            "id" => id,
+        }).expect("Query failed.");
+
+        //if the parent does not exist return false if it does exist return true
+        if res.len() == 0{
+            Ok(false)
+        }else{
+            Ok(true)
+        } 
+    }
+
+    //verify a token for given ip address and uid
+    pub fn  verify_token(token:String, ip:String, uid_test:u64)-> Result<bool>{
+        //initialize connection
+        let opts = Opts::from_url(URL).unwrap();
+        let pool = Pool::new(opts).unwrap();
+        let mut conn = pool.get_conn().unwrap();
+
         log::info!("testing ip {} with token {}", ip.clone(), token.clone());
+
+        //prepare sql query
         let mut stmt = conn.prep("SELECT auth_id FROM auth WHERE active_token = :token AND token_ip = :ip")?;
+
+        //execute query
         let aid_vec:Vec<u64> = conn.exec(stmt, params!{
             "token" => token.clone(),
             "ip" => ip_tools::strip_port(ip.clone()),
         }).expect("Query failed.");
+
+        //if there are no entries for this ip token combo then this is a bad token
         if aid_vec.len() != 1{
             log::info!("Token failed for ip {}", ip.clone());
             return Ok(false);
         }
+        //get the auth id of the token ip combo
         let aid:u64 = aid_vec[0];
         log::info!("testing aid {}", aid);
+
+        //prepare sql query
         stmt = conn.prep("SELECT user_id FROM users WHERE auth_id = :aid")?; 
+
+        //execute the statement
         let uid_vec:Vec<u64> = conn.exec(stmt, params!{
             "aid" => aid,
         }).expect("Query Failed");
+
+        //if there are no users with that aid then return false
         if aid_vec.len() != 1{
             log::info!("UID failed for ip {}", ip.clone());
             return Ok(false);
         }
+        //get the uid with the matching auth id
         let uid:u64 = uid_vec[0];
+
+        //if the given uid and the uid on the server match then it passes the test
         if uid == uid_test{
             log::info!("UID and Token passed for ip {}", ip.clone());
             return Ok(true);
         }
+
+        //return false if above does not pass
         Ok(false)
     }
 }
--- src/database/insert.rs
+++ src/database/insert.rs
@@ -14,14 +14,25 @@ pub mod insert{
 
     const URL:&str = "mysql://server:serverpass@10.16.40.202:3306/forum";
 
-    pub fn add_user(newuser: database::NewUser) -> Result<String>{//idk what this should be returning realistically
+    //create a new user
+    pub fn add_user(newuser: database::NewUser) -> Result<String>{//return userid
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //create the auth entry for user
         let auth: Auth = create::create_auth(newuser.clone());
+
+        //create the mod record for user
         let modrec: ModerationRecord = create::create_mod_rec();
+
+        //create the user object
         let user: User = create::create_user(newuser.clone(), auth.auth_id, modrec.moderation_id);
+
+        //prepare the sql statement
         let stmt = conn.prep("INSERT INTO users (username,locale,auth_id, moderation_id) VALUES(:uname,:locale,:auth_id,:mod_id)")?;
+        //execute the statement
         conn.exec_drop(&stmt, params!{
             "uname" => user.username.clone(),
             "locale" => user.locale.clone(),
@@ -29,13 +40,19 @@ pub mod insert{
             "mod_id" => user.moderation_id,
         },).unwrap();
 
-        Ok("user added".to_owned())
+        //return the userid
+        Ok(conn.last_insert_id().to_string().to_owned())
     }
+    //insert auth account for user
     pub fn add_auth(new_auth: &Auth) -> Result<u64>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //prepare sql statement
         let stmt = conn.prep("INSERT INTO auth (creation_ip, last_ip, email, pwhash, uname, last_change, created) VALUES(:creation_ip, :last_ip, :email, :pwhash, :uname, :last_change, :created)")?;
+        //execute statement
         conn.exec_drop(&stmt, params!{
             "creation_ip" => ip_tools::strip_port(new_auth.creation_ip.clone()),
             "last_ip" => ip_tools::strip_port(new_auth.last_ip.clone()),
@@ -46,60 +63,89 @@ pub mod insert{
             "created" => new_auth.created,
         },).unwrap();
 
+        //return the auth id
         Ok(conn.last_insert_id())
     }
+
+    //create a moderation record for user
     pub fn add_modrec() -> Result<u64>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //prepare sql statement
         let stmt = conn.prep("INSERT INTO user_moderation (infraction_counter) VALUES(:infraction_counter)")?;
+
+        //execute statement
         conn.exec_drop(&stmt, params!{
             "infraction_counter" => "0",
         },).unwrap();
-        //do something with this
-
+        
+        //return the moderation id
         Ok(conn.last_insert_id())
     }
 
     //insert a new token into the database for user with aid u64
     pub fn add_token(token: String, aid: u64, ip: String) -> Result<bool>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+
+        //get the current date
         let date = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
+        //add the token for aid
         log::info!("adding token {} to auth for aid {}", token, aid);
         let stmt = conn.prep("UPDATE auth SET active_token = :token, token_ip = :ip, token_date = :date WHERE auth_id = :aid")?;
+
+        //execute statement
         conn.exec_drop(&stmt, params!{
             "token" => token,
             "aid" => aid,
             "ip" => ip_tools::strip_port(ip),
             "date" => date,
         },).unwrap();
+        //return true
         Ok(true)
     }
+    //create a new category
     pub fn create_cat(cat: MakeCat) -> Result<u64>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+        //get the current date
         let date = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
         log::info!("adding cat {} to categories for uid {}", cat.name.clone(), cat.user);
+        //prepare the sql statement
         let stmt = conn.prep("INSERT INTO categories (creator_id, cat_name, cat_desc, creation_date) VALUES(:uid, :name, :desc, :date)")?;
+        //execute statement
         conn.exec_drop(&stmt, params!{
             "date" => date,
             "desc" => cat.desc.clone(),
             "uid" => cat.user,
             "name" => cat.name.clone(),
         },).unwrap();
+        //get cat id
         let cat_id:u64 = conn.last_insert_id();
+        //return the category id
         Ok(cat_id)
     }
+    //create a new post
     pub fn create_post(post: MakePost) -> Result<u64>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+        //get the current date
         let date = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
         log::info!("adding post {} to category {} for uid {}", post.name.clone(), post.cat, post.user);
+        //prepare statement
         let stmt = conn.prep("INSERT INTO posts (creator_id, cat_id, content, name, creation_date) VALUES(:uid, :cat_id, :content, :name, :date)")?;
+        //execute statement
         conn.exec_drop(&stmt, params!{
             "cat_id" => post.cat,
             "date" => date,
@@ -107,16 +153,23 @@ pub mod insert{
             "uid" => post.user,
             "name" => post.name.clone(),
         },).unwrap();
+        //get the post id
         let post_id:u64 = conn.last_insert_id();
+        //return the post id
         Ok(post_id)
     }
+    //create a new comment
     pub fn create_comment(comment: MakeComment) -> Result<u64>{
+        //initialize connection
         let opts = Opts::from_url(URL).unwrap();
         let pool = Pool::new(opts).unwrap();
         let mut conn = pool.get_conn().unwrap();
+        //get the current date
         let date = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
         log::info!("adding comment to post {} for uid {}", comment.post, comment.user);
+        //prepare sql statement
         let stmt = conn.prep("INSERT INTO comments (creator_id, post_id, parent_id, content, creation_date) VALUES(:uid, :post_id, :parent_id, :content, :date)")?;
+        //execute sql statement
         conn.exec_drop(&stmt, params!{
             "post_id" => comment.post,
             "date" => date,
@@ -124,7 +177,9 @@ pub mod insert{
             "uid" => comment.user,
             "parent_id" => comment.parent,
         },).unwrap();
+        //get comment id
         let comment_id:u64 = conn.last_insert_id();
+        //return comment id
         Ok(comment_id)
     }
 }
